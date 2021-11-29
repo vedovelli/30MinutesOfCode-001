d0d21ae0aa4976be94b65c3bb68a8a1df1ba9859
Yann Braga <yannbf@gmail.com>
2021-11-29 17:43:43 +0100

    Revert "Replace React Query with React hooks"
    This reverts commit bc2027abf933a4606eb3d48a578c216eb8e45f6e.

--- src/containers/Products/Products.js
+++ src/containers/Products/Products.js
@@ -1,32 +1,22 @@
 import { http } from "../../service/api";
 import { Card } from "./Card";
-import { Header } from "../../components";
-// import { useQuery } from "react-query";
-import { useState, useEffect } from "react";
+import { Header, Spinner } from "../../components";
+import { useQuery } from "react-query";
 
 export default function Products() {
-  const [products, setProducts] = useState([]);
+  const { isLoading, isError, data } = useQuery(
+    "todos",
+    () => http.get("/products").then(({ data }) => data.products) // http container baseURL with `/api`
+  );
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await http.get("/products");
-      setProducts(data?.products ?? []);
-    };
-    fetchProducts();
-  }, []);
-  // const { isLoading, isError, data } = useQuery(
-  //   "todos",
-  //   () => http.get("/products").then(({ data }) => data.products) // http container baseURL with `/api`
-  // );
-
-  // if (isLoading) return <Spinner message="Loading Products" variant="orange" />;
-  // if (isError) return <p>Error</p>;
+  if (isLoading) return <Spinner message="Loading Products" variant="orange" />;
+  if (isError) return <p>Error</p>;
 
   return (
     <>
       <Header>Products</Header>
       <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        {products.map((product) => (
+        {data.map((product) => (
           <Card product={product} key={product.id} />
         ))}
       </div>
